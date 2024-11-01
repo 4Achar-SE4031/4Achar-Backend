21590e71fa425cb8304000f5318bd449a94007f6
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-01 22:23:59 +0330

    feat: IAccountService interface

---
+++ src/Concertify.Domain/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+using Concertify.Domain.Dtos.Account;
+
+
+namespace Concertify.Domain.Interfaces;
+
+public interface IAccountService
+{
+    public Task<List<Claim>> GetToken(UserLoginDto loginDto);
+    public Task<string> RegisterUser(UserRegisterDto registerDto);
+}
