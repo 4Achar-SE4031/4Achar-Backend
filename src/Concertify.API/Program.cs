f9d41db7bc97727bb4c280f9e16dbff711c34807
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-01 22:30:08 +0330

    feat: startup configuration

--- src/Concertify.API/Program.cs
+++ src/Concertify.API/Program.cs
@@ -1,3 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Concertify.Domain.Models;
+using Concertify.Application;
+using Concertify.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.OpenApi.Models;
+
 
 namespace Concertify.API
 {
@@ -7,12 +18,79 @@ namespace Concertify.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             // Add services to the container.
+            DependencyInjectionConfiguration.RegisterServices(builder.Services);
+
+            string connectionString = builder.Configuration.GetConnectionString("SqlServerDatabase");
+
+            builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddJwtBearer(options =>
+           {
+               options.SaveToken = true;
+               options.RequireHttpsMetadata = false;
+               options.TokenValidationParameters = new TokenValidationParameters()
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                   ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+               };
+           });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("LoggedInUsersOnly", policy =>
+                    policy.RequireAuthenticatedUser());
+            });
+
+            builder.Services.Configure<IdentityOptions>(options =>
+                options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier);
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Product Catalogue", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
 
             var app = builder.Build();
 
@@ -21,11 +99,21 @@ namespace Concertify.API
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.MapSwagger().RequireAuthorization();
+                app.UseDeveloperExceptionPage();
             }
+
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                //dbContext.Database.EnsureDeleted();
+                //dbContext.Database.Migrate();
+            }   
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
 
             app.MapControllers();
