1265a69834eb69aba955f87a5b8ef385ceb9cbda
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-01 22:26:04 +0330

    feat: register services in DI container

---
+++ src/Concertify.Application/DependencyInjectionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Concertify.Domain.Interfaces;
+using Concertify.Infrastructure.ExternalServices;
+using Concertify.Application.Services;
+
+
+namespace Concertify.Application;
+
+public class DependencyInjectionConfiguration
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(DtoEntityMapperProfile));
+
+        services.AddScoped<IAccountService, AccountService>();
+    }
+        
+}
