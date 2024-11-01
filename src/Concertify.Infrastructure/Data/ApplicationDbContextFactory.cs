68a25b1c3c0b2067d4d0d6868b9974e0a3d409c5
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-01 22:27:21 +0330

    feat: implement dbcontext factory

---
+++ src/Concertify.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concertify.Infrastructure.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ProductCatalogue;Integrated Security=True;";
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString)
+                .EnableSensitiveDataLogging();
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
