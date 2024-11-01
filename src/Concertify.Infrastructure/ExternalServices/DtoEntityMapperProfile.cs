2ba6ee791b7a702d8309215f171e7917b8e14079
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-01 22:28:06 +0330

    feat: AutoMapper profile

---
+++ src/Concertify.Infrastructure/ExternalServices/DtoEntityMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using Concertify.Domain.Dtos.Account;
+using Concertify.Domain.Models;
+using AutoMapper;
+
+namespace Concertify.Infrastructure.ExternalServices;
+
+public class DtoEntityMapperProfile : Profile
+{
+    public DtoEntityMapperProfile()
+    {
+        CreateMap<UserRegisterDto, ApplicationUser>()
+           .ForSourceMember(src => src.Password, opt => opt.DoNotValidate());
+    }
+}
