e5a127213d30eddf6a9fd22cc9108ce29a3e0389
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-11 16:39:59 +0330

    feat: implement email confirmation logic

--- src/Concertify.Application/Services/AccountService.cs
+++ src/Concertify.Application/Services/AccountService.cs
@@ -1,13 +1,15 @@
-﻿using System.Security.Claims;
+﻿using System.Data;
 using System.IdentityModel.Tokens.Jwt;
-using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
 using AutoMapper;
 
 using Concertify.Domain.Dtos.Account;
 using Concertify.Domain.Interfaces;
 using Concertify.Domain.Models;
-using System.Data;
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace Concertify.Application.Services;
 
@@ -15,11 +17,13 @@ public class AccountService : IAccountService
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IMapper _mapper;
+    private readonly IEmailSender _emailSender;
     
-    public AccountService(UserManager<ApplicationUser> userManager, IMapper mapper)
+    public AccountService(UserManager<ApplicationUser> userManager, IMapper mapper, IEmailSender emailSender)
     {
         _userManager = userManager;
         _mapper = mapper;
+        _emailSender = emailSender;
     }
 
     public async Task<List<Claim>> GetToken(UserLoginDto loginDto)
@@ -46,12 +50,8 @@ public class AccountService : IAccountService
     }
 
 
-    public async Task<string> RegisterUser(UserRegisterDto registerDto)
+    public async Task<UserInfoDto> RegisterUser(UserRegisterDto registerDto)
     {
-        var existingUser = await _userManager.FindByNameAsync(registerDto.UserName);
-        if (existingUser is not null)
-            throw new DuplicateNameException($"A user with the username {registerDto.UserName} already exists");
-
         ApplicationUser newUser = _mapper.Map<ApplicationUser>(registerDto);
 
         var result = await _userManager.CreateAsync(newUser, registerDto.Password);
@@ -59,6 +59,48 @@ public class AccountService : IAccountService
         if (!result.Succeeded)
             throw new Exception(result.Errors.First().Description);
 
-        return newUser.Id;
+        ApplicationUser createdUser = await _userManager.FindByNameAsync(newUser.UserName!)
+            ?? throw new Exception("Internal server error");
+
+        await SendConfirmationEmailAsync(createdUser);
+
+        UserInfoDto userInfo = new()
+        {
+            FirstName = createdUser.FirstName!,
+            LastName = createdUser.LastName!,
+            Email = createdUser.Email!,
+            UserName = createdUser.UserName!,
+        };
+
+        return userInfo;
     }
+
+    public async Task ConfirmEmail(string email, string confirmationToken)
+    {
+        ApplicationUser user = await _userManager.FindByEmailAsync(email)
+            ?? throw new Exception("Unexpected error occured!");
+
+        var result = await _userManager.ConfirmEmailAsync(user, confirmationToken);
+
+        if (!result.Succeeded)
+            throw new Exception("Unexpected error occured!");
+    }
+
+    private async Task SendConfirmationEmailAsync(ApplicationUser user)
+    {
+        var confirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        var confirmationLink = "http://localhost:5000/Account/confirm_email/";
+        var emailContent = $"Thanks for subscribing to Concertify!" +
+        $"<br/><br/>" +
+        $"To activate your email, please click on the following link: " +
+        $"<br/><br/>" +
+        $"<a href=\"{confirmationLink}\">Activation Link</a>" +
+        $"<br/><br/>" +
+        $"<a href=\"{confirmationLink}\">{confirmationLink}</a>" +
+        $"<br/><br/>" +
+        $"Concertify Team";
+
+        await _emailSender.SendEmailAsync(user.Email!, "Concertify team. Confirm your email", emailContent);
+    }
+
 }
