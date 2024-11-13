ba4c2c6b65941a8b0d97bb37376d41f4fdb65f33
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-13 15:43:25 +0330

    feat: implement custom TOTP token provider class

---
+++ src/Concertify.Application/Services/CustomTotpTokenProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Concertify.Application.Services;
+
+public class CustomTotpTokenProvider<TUser> : TotpSecurityStampBasedTokenProvider<TUser> where TUser : class
+{
+    public override async Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<TUser> manager, TUser user)
+    {
+        var isTwoFactorEnabled = await manager.GetTwoFactorEnabledAsync(user);
+        if (!isTwoFactorEnabled)
+            return false;
+        
+        var securityStamp = await manager.GetSecurityStampAsync(user);
+        return !string.IsNullOrEmpty(securityStamp);
+    }
+
+}
