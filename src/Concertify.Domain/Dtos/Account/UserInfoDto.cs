7a0d95fb8e8496ed0c35d9b0cbdde49c79caf21c
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-13 17:48:05 +0330

    refactor: general refactoring

--- src/Concertify.Domain/Dtos/Account/UserInfoDto.cs
+++ src/Concertify.Domain/Dtos/Account/UserInfoDto.cs
@@ -2,8 +2,8 @@
 
 public record UserInfoDto
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string UserName { get; set; }
-    public string Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string UserName { get; set; } = default!;
+    public string Email { get; set; } = default!;
 }
