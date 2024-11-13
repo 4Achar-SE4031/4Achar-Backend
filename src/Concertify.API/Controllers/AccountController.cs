b94aca501ebbbf653e65688ec86928f85be8e7d8
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-13 17:46:32 +0330

    feat: added (update, retrieve) user info, change password endpoints

--- src/Concertify.API/Controllers/AccountController.cs
+++ src/Concertify.API/Controllers/AccountController.cs
@@ -1,11 +1,13 @@
-﻿using Concertify.Domain.Dtos.Account;
-using Concertify.Domain.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
+using Concertify.Domain.Dtos.Account;
+using Concertify.Domain.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+    
 namespace Concertify.API.Controllers;
 
 [ApiController]
@@ -23,14 +25,14 @@ public class AccountController : ControllerBase
 
     [HttpPost]
     [Route("jwt/create")]
-    public async Task<IActionResult> GetToken(UserLoginDto loginDto)
+    public async Task<IActionResult> GetTokenAsync(UserLoginDto loginDto)
     {
-        List<Claim> userClaims = await _accountService.GetToken(loginDto);
+        List<Claim> userClaims = await _accountService.GetTokenAsync(loginDto);
         SymmetricSecurityKey authSigningKey = new(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
 
         JwtSecurityToken token = new(
-            issuer: null,
-            audience: null,
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
             expires: DateTime.Now.AddDays(1),
             claims: userClaims,
             signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
@@ -47,16 +49,16 @@ public class AccountController : ControllerBase
 
     [HttpPost]
     [Route("signup")]
-    public async Task<IActionResult> Register(UserRegisterDto registerDto)
+    public async Task<IActionResult> RegisterAsync(UserRegisterDto registerDto)
     {
-        UserInfoDto userInfo = await _accountService.RegisterUser(registerDto);
+        UserInfoDto userInfo = await _accountService.RegisterUserAsync(registerDto);
 
         return Ok(userInfo);
     }
 
     [HttpPost]
     [Route("confirm_email")]
-    public async Task<IActionResult> ConfirmEmail([FromQuery] string email, [FromQuery] string confirmationToken)
+    public async Task<IActionResult> ConfirmEmailAsync([FromQuery] string email, [FromQuery] string confirmationToken)
     {
         if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(confirmationToken))
         {
@@ -66,11 +68,55 @@ public class AccountController : ControllerBase
             });
         }
 
-        await _accountService.ConfirmEmail(email, confirmationToken);
+        await _accountService.ConfirmEmailAsync(email, confirmationToken);
 
         return Ok(new
         {
             detail = "Your email was confirmed successfully!"
+        });
+    }
+
+    [HttpGet]
+    [Route("me")]
+    public async Task<IActionResult> GetUserInfoAsync()
+    {
+        if (!User.Identity.IsAuthenticated)
+            return Unauthorized();
+
+        string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("User Id cannot be null.");
+
+        var info = await _accountService.GetUserInfoAsync(userId);
+
+        return Ok(info);
+
+    }
+
+    [HttpPut]
+    [Route("me/update")]
+    public async Task<IActionResult> UpdateUserInfoAsync(UserUpdateDto updateDto)
+    {
+        string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("User Id cannot be null.");
+
+        var updatedInfo = await _accountService.UpdateUserInfoAsync(updateDto, userId);
+
+        return Ok(updatedInfo);
+
+    }
+
+    [HttpPut]
+    [Route("me/change_password")]
+    public async Task<IActionResult> ChangePasswordAsync(ChangePasswordDto changePassword)
+    {
+        string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+               ?? throw new Exception("User Id cannot be null.");
+
+        await _accountService.ChangeUserPasswordAsync(changePassword, userId);
+
+        return Ok(new
+        {
+            detail = "Your password was changed successfully!"
         });
     }
 }
