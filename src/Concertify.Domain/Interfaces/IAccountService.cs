22a8dd48e8237b72ae46b5f25dc7e1301125a2b0
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-13 17:47:43 +0330

    feat: add needed methods

--- src/Concertify.Domain/Interfaces/IAccountService.cs
+++ src/Concertify.Domain/Interfaces/IAccountService.cs
@@ -8,7 +8,12 @@ namespace Concertify.Domain.Interfaces;
 
 public interface IAccountService
 {
-    public Task<List<Claim>> GetToken(UserLoginDto loginDto);
-    public Task<UserInfoDto> RegisterUser(UserRegisterDto registerDto);
-    public Task ConfirmEmail(string email, string confirmationToken);
+    public Task<List<Claim>> GetTokenAsync(UserLoginDto loginDto);
+    public Task<UserInfoDto> RegisterUserAsync(UserRegisterDto registerDto);
+    public Task ConfirmEmailAsync(string email, string confirmationToken);
+    public Task SendConfirmationEmailAsync(ApplicationUser user);
+    public Task<UserInfoDto> GetUserInfoAsync(string userId);
+    public Task<UserInfoDto> UpdateUserInfoAsync(UserUpdateDto userUpdate, string userId);
+    public Task ChangeUserPasswordAsync(ChangePasswordDto changePasswordDto, string userId);
+
 }
