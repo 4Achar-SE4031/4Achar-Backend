04b1f3669bfd01b1d09fcf6a023b21a57ce81359
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-13 17:49:21 +0330

    feat: change password DTO

---
+++ src/Concertify.Domain/Dtos/Account/ChangePasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concertify.Domain.Dtos.Account;
+
+public record ChangePasswordDto
+{
+    [Required]
+    public string OldPassword { get; set; } = default!;
+    [Required]
+    public string NewPassword { get; set; } = default!;
+}
