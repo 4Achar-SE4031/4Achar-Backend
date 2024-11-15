75b89003de5abaf4b6a86041a7e73c7f62c8ece9
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-15 12:34:20 +0330

    feat: email confirmation dto

---
+++ src/Concertify.Domain/Dtos/Account/EmailConfirmationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concertify.Domain.Dtos.Account;
+
+public record EmailConfirmationDto
+{
+    [Required]
+    public string ConfirmationToken{ get; set; } = default!;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = default!;
+}
