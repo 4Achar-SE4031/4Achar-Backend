cfb7e6f0fd5e7f0a344e2c54a46827c8e67c5231
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-15 18:46:57 +0330

    chore: password reset email request DTO

---
+++ src/Concertify.Domain/Dtos/Account/PasswordResetEmailRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concertify.Domain.Dtos.Account;
+
+public record PasswordResetEmailRequestDto
+{
+    [Required]
+    public string Email { get; set; } = default!;
+}
