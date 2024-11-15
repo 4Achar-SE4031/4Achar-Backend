243a301dc43484a4bae2e211e979dc0bab2677a0
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-15 18:47:18 +0330

    chore: password reset DTO

---
+++ src/Concertify.Domain/Dtos/Account/UserPasswordResetDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concertify.Domain.Dtos.Account;
+
+public record UserPasswordResetDto
+{
+    [Required]
+    public string UserEmail { get; set; } = default!;
+    [Required]
+    public string PasswordResetToken { get; set; } = default!;
+    [Required]
+    public string NewPassword { get; set; } = default!;
+    [Required]
+    public string ConfirmNewPassword {  get; set; } = default!;
+}
