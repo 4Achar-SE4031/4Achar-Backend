27f64fc93764ea2f546499d2534c57469d3a9598
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 20:24:37 +0330

    chore: modify fields

--- src/Concertify.Domain/Dtos/Concert/ConcertSummaryDto.cs
+++ src/Concertify.Domain/Dtos/Concert/ConcertSummaryDto.cs
@@ -2,6 +2,7 @@
 
 public record ConcertSummaryDto
 {
+    public int Id { get; set; }
     public string Title { get; set; } = default!;
     public string StartDate { get; set; } = default!;
     public string? City { get; set; }
