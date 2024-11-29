bbb9a87393f3130fe55dbb23f7ab0fbe8cab4491
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 20:23:07 +0330

    feat: add Web Scraper interface

---
+++ src/Concertify.Domain/Interfaces/IWebScraper.cs
@@ -0,0 +1,9 @@
+﻿using Concertify.Domain.Models;
+
+namespace Concertify.Domain.Interfaces;
+
+public interface IWebScraper
+{
+    public List<string> ExtractLinks(string url);
+    public Concert Scrape(string url);
+}
