ad1d5a8ea6bbfe772661a81be050aa7a57e4657c
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 11:36:32 +0330

    feat: implement Concert model

---
+++ src/Concertify.Domain/Models/Concert.cs
@@ -0,0 +1,15 @@
+﻿namespace Concertify.Domain.Models;
+
+public class Concert : EntityBase
+{
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string StartDate { get; set; } = default!;
+    public string? Province { get; set; }
+    public string? City { get; set; }
+    public string Address { get; set; } = default!;
+    public string Category { get; set; } = default!;
+    public string TicketPrice { get;set; } = default!;
+    public float Latitude { get; set; } = default!;
+    public float Longitude { get; set; } = default!;
+}
