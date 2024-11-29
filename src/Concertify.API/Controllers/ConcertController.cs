a0c92ab3d42944cef0da5660d610234ff4039075
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 11:40:28 +0330

    feat: implement Concert controller

---
+++ src/Concertify.API/Controllers/ConcertController.cs
@@ -0,0 +1,39 @@
+﻿using Concertify.Domain.Dtos.Concert;
+using Concertify.Domain.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Concertify.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConcertController : ControllerBase
+{
+    private readonly IConcertService _concertService;
+
+    public ConcertController(IConcertService concertService)
+    {
+        _concertService = concertService;
+    }
+
+    [HttpGet]
+    [Produces(typeof(List<ConcertSummaryDto>))]
+    public async Task<IActionResult> GetConcertsAsync([FromQuery] ConcertFilterDto concertFilter)
+    {
+        List<ConcertSummaryDto> concerts = await _concertService.GetConcertsAsync(concertFilter);
+
+        return Ok(concerts);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [Produces(typeof(ConcertDetailsDto))]
+    public async Task<IActionResult> GetConcertByIdAsync(int id)
+    {
+        ConcertDetailsDto concert = await _concertService.GetConcertByIdAsync(id);
+
+        return Ok(concert);
+    }
+
+}
