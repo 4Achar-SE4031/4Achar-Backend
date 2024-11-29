30cba70580f0b84bd935cb0f64a35fdd8ff05052
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 11:37:24 +0330

    feat: Generic Repository interface

---
+++ src/Concertify.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+using Concertify.Domain.Models;
+
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Concertify.Domain.Interfaces;
+
+public interface IGenericRepository<T> where T : EntityBase
+{
+    public Task<List<T>> GetFilteredAsync(Expression<Func<T, bool>>[] filtered,
+        int? skip,
+        int? take,
+        params Expression<Func<T, bool>>[] includes);
+
+    public Task<List<T>> GetAsync(int? skip, int? take, params Expression<Func<T, bool>>[] includes);
+    public Task<T> GetByIdAsync(int id, params Expression<Func<T, bool>>[] includes);
+    public Task<int> InsertAsync (T entity);
+    public void Update (T entity);
+    public void Delete(T entity);
+    public Task SaveChangesAsync();
+}
