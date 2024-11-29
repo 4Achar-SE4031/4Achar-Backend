603857d05c89b91685419137f87e033b9f8d2d3c
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 20:23:56 +0330

    feat: implement scraper controller

---
+++ src/Concertify.API/Controllers/ScrapeController.cs
@@ -0,0 +1,23 @@
+﻿using Concertify.Domain.Dtos.Concert;
+using Concertify.Domain.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Concertify.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ScrapeController(IScraperService scraperService) : ControllerBase
+{
+    private readonly IScraperService _scraperService = scraperService;
+
+    [HttpGet]
+    [Route("collect")]
+    [Produces(typeof(List<ConcertSummaryDto>))]
+    public async Task<IActionResult> Collect()
+    {
+        List<ConcertSummaryDto> concerts = await _scraperService.Collect();
+
+        return Ok(concerts);
+    }
+}
