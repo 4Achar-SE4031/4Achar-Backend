7348a68d6008130c0b8031b4800f642ed3096923
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 20:24:19 +0330

    chore: register services in DI container

--- src/Concertify.Application/DependencyInjectionConfiguration.cs
+++ src/Concertify.Application/DependencyInjectionConfiguration.cs
@@ -3,9 +3,13 @@ using Concertify.Domain.Interfaces;
 using Concertify.Domain.Models;
 using Concertify.Infrastructure.Data;
 using Concertify.Infrastructure.ExternalServices;
+using Concertify.Scraper.Scrapers;
 
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.DependencyInjection;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 
 namespace Concertify.Application;
@@ -20,6 +24,18 @@ public class DependencyInjectionConfiguration
         services.AddScoped<IEmailSender, EmailSender>();
         services.AddScoped<IConcertService, ConcertService>();
         services.AddScoped<IGenericRepository<Concert>, GenericRepository<Concert>>();
+        
+        services.AddScoped<IScraperService, ScraperService>();
+        services.AddScoped<IWebScraper, HonarTicketScraper>();
+        services.AddScoped<IWebDriver, ChromeDriver>(sp =>
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--ingore-ssl-errors=yes",
+                "--ignore-certificate-errors",
+                "--no-sandbox",
+                "--headless");
+            return ActivatorUtilities.CreateInstance<ChromeDriver>(sp, options);
+        });
     }
         
 }
