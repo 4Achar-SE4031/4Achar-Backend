01c376d20426d32f80f0aff8c510a4bf2ab0461f
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 11:40:50 +0330

    refactor: general refacatoring

--- src/Concertify.Infrastructure/Data/ApplicationDbContextFactory.cs
+++ src/Concertify.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -1,21 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Concertify.Infrastructure.Data
+namespace Concertify.Infrastructure.Data;
+
+public class ApplicationDbContextFactory : IDbContextFactory<ApplicationDbContext>
 {
-    public class ApplicationDbContextFactory : IDbContextFactory<ApplicationDbContext>
+    public ApplicationDbContext CreateDbContext()
     {
-        public ApplicationDbContext CreateDbContext()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            
-            return new ApplicationDbContext(optionsBuilder.Options);
-            
-        }
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        
+        return new ApplicationDbContext(optionsBuilder.Options);
+        
     }
 }
