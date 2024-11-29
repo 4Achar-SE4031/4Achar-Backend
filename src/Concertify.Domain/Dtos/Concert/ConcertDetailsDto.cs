9e219c98c2a294f587eb57684c23447a96c004a5
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-11-29 18:00:13 +0330

    refactor: change properties

--- src/Concertify.Domain/Dtos/Concert/ConcertDetailsDto.cs
+++ src/Concertify.Domain/Dtos/Concert/ConcertDetailsDto.cs
@@ -5,11 +5,12 @@ public record ConcertDetailsDto
     public int Id { get; set; }
     public string Title { get; set; } = default!;
     public string Description { get; set; } = default!;
-    public DateTime StartDate { get; set; }
+    public string StartDate { get; set; } = default!;
     public string? Province { get; set; }
     public string? City { get; set; }
     public string Address { get; set; } = default!;
     public string Category { get; set; } = default!;
     public string TicketPrice { get; set; } = default!;
-
+    public float Latitude {  get; set; } = default!;
+    public float Longitude { get; set; } = default!;
 }
--- src/Concertify.Domain/Dtos/Concert/ConcertSummaryDto.cs
+++ src/Concertify.Domain/Dtos/Concert/ConcertSummaryDto.cs
@@ -3,7 +3,7 @@
 public record ConcertSummaryDto
 {
     public string Title { get; set; } = default!;
-    public DateTime StartDate { get; set; }
+    public string StartDate { get; set; } = default!;
     public string? City { get; set; }
     public string Category { get; set; } = default!;
     public string TicketPrice { get; set; } = default!;
