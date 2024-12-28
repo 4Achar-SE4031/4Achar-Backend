a2e2b89c369b02db1a11fe0938abe718ea3b69a6
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-12-28 15:46:10 +0330

    feat: implement custom exceptions

---
+++ src/Concertify.Domain/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+
+namespace Concertify.Domain.Exceptions;
+
+[Serializable]
+public class ItemNotFoundException : Exception
+{
+    public readonly int? ItemId;
+    public ItemNotFoundException(int itemId)
+    {
+        ItemId = itemId;
+    }
+
+    public ItemNotFoundException(string? message) : base(message)
+    {
+    }
+
+    public ItemNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected ItemNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
