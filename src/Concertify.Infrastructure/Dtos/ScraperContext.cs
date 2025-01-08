2a89fbe137990cb6a20240a084498052b76065ed
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-09 00:53:46 +0330

    feat: ScraperContext DTO

---
+++ src/Concertify.Infrastructure/Dtos/ScraperContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Concertify.Infrastructure.Dtos;
+
+public record ScraperContext
+{
+    public string Url { get; init; } = default!;
+    public string City { get; init; } = default!;
+    public string CardImage { get; init; } = default!;
+}
