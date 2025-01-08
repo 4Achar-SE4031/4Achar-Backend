7f453128dad8390e885791c364730fb38ba15b3a
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-09 00:56:26 +0330

    refactor: modify fields

--- src/Concertify.Domain/Models/Concert.cs
+++ src/Concertify.Domain/Models/Concert.cs
@@ -4,12 +4,16 @@ public class Concert : EntityBase
 {
     public string Title { get; set; } = default!;
     public string Description { get; set; } = default!;
-    public string StartDate { get; set; } = default!;
-    public string? Province { get; set; }
+    public DateTime StartDateTime { get; set; } = default!;
     public string? City { get; set; }
+    public string Location { get; set; } = default!;
     public string Address { get; set; } = default!;
     public string Category { get; set; } = default!;
     public string TicketPrice { get;set; } = default!;
     public float Latitude { get; set; } = default!;
     public float Longitude { get; set; } = default!;
+    public string CoverImage { get; set;} = default!;
+    public string CardImage { get; set; } = default!;
+    public string Url { get; set; } = default!;
+
 }
