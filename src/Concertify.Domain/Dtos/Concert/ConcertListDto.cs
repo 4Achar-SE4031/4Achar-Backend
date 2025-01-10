98dd1dc4179e5fb43d5ac04f01fe08b07dc1bab6
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-10 17:49:36 +0330

    feat: concert list DTO

---
+++ src/Concertify.Domain/Dtos/Concert/ConcertListDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Concertify.Domain.Dtos.Concert;
+
+public record ConcertListDto
+{
+    public List<ConcertSummaryDto> Concerts { get; set; } = default!;
+    public int TotalCount { get; set; } = default;
+}
