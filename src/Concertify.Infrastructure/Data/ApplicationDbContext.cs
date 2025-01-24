8850d88e5f08e515b693c1a7bb470b63fdd8862b
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-24 10:32:09 +0330

    feat: add unique constraint for concert

--- src/Concertify.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Concertify.Infrastructure/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace Concertify.Infrastructure.Data;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<Concert> Concerts { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -27,12 +28,32 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         base.OnModelCreating(builder);
 
         builder.Entity<Concert>().HasKey(e => e.Id);
+        builder.Entity<Rating>().HasKey(e => e.Id);
+
+        builder.Entity<Rating>()
+            .HasIndex(r => new {r.UserId, r.ConcertId })
+            .IsUnique();
+
+        builder.Entity<Concert>()
+            .HasIndex(c => new
+            {
+                c.Title,
+                c.StartDateTime,
+                c.City,
+            }).IsUnique();
+
+        builder.Entity<Concert>()
+            .HasMany(e => e.Ratings)
+            .WithMany(e => e.RatedConcerts)
+            .UsingEntity<Rating>(
+                l => l.HasOne<ApplicationUser>().WithMany().HasForeignKey(e => e.UserId),
+                r => r.HasOne<Concert>().WithMany().HasForeignKey(e => e.ConcertId));
+        
         builder.Entity<Concert>()
             .Property(c => c.StartDateTime)
             .HasConversion(
                 src => src.Kind == DateTimeKind.Utc ? src : DateTime.SpecifyKind(src, DateTimeKind.Utc),
                 dest => dest.Kind == DateTimeKind.Utc ? dest : DateTime.SpecifyKind(dest, DateTimeKind.Utc)
             );
-        
     }
 }
--- src/Concertify.Infrastructure/ExternalServices/Scrapers/ScraperManager.cs
+++ src/Concertify.Infrastructure/ExternalServices/Scrapers/ScraperManager.cs
@@ -3,6 +3,8 @@ using Concertify.Domain.Models;
 using Concertify.Infrastructure.Data;
 using Concertify.Infrastructure.Interfaces;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Concertify.Infrastructure.ExternalServices.Scrapers;
 
 public class ScraperManager(IWebScraper scraper, ApplicationDbContext context) : IScraperManager
@@ -14,6 +16,9 @@ public class ScraperManager(IWebScraper scraper, ApplicationDbContext context) :
     {
         await foreach (var concert in _scraper.ExtractLinks(url))
         {
+            bool exists = await _context.Concerts.AnyAsync(c => c.Title == concert.Title && c.StartDateTime == concert.StartDateTime && c.City == concert.City);
+            if (exists)
+                continue;
             await _context.Concerts.AddAsync(concert);
             await _context.SaveChangesAsync();
             yield return concert;
