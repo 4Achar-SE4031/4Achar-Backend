c5352c0f874ea549fc1d661e897fadfcedd725b7
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-27 14:16:43 +0330

    chore: migrations

--- src/Concertify.Infrastructure/Migrations/20250127102213_BookmarkModel.Designer.cs
+++ src/Concertify.Infrastructure/Migrations/20250127102213_BookmarkModel.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Concertify.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Concertify.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250127102213_BookmarkModel")]
+    partial class BookmarkModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
